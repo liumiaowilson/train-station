6ddf9c65aca702755ad1877a06fc912416cdf555
Miao (Wilson) Liu <wilson@clicktocloud.com>
2017-08-29 14:48:22 +1000

    simplify example stop script

--- services/example/stop.sh
+++ services/example/stop.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 port=$1
 
-ps aux | grep -v grep | grep -q "\-p $port"
-if [[ $? -eq 0 ]]; then
-    kill -9 `ps aux | grep "p $port" | grep -v grep | awk '{ print $2 }'`
-fi
+kill -9 `ps aux | grep "p $port" | grep -v grep | awk '{ print $2 }'`
